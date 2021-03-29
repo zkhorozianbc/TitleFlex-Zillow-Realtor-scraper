b24c34c056e4bf2007513e5f0d244e3bb71f1eb7
Zachary Khorozian <zkhorozianbc@gmail.com>
2021-03-29 12:12:52 -0400

    changed class field names

--- FirefoxDriver.py
+++ FirefoxDriver.py
@@ -13,30 +13,30 @@ import os
 class FirefoxDriver:
 	
 	@property
-	def ff_driver_path(self):
+	def firefox_driver_path(self):
 		path = os.path.join("usr/local/bin/geckodriver")
 		return path
 
 	@property
-	def ff_profile(self):
+	def firefox_profile(self):
 
-		ff_profile = webdriver.FirefoxProfile()
-		ff_profile.set_preference("browser.download.folderList", 2)
-		ff_profile.set_preference("browser.download.manager.showWhenStarting", False)
-		ff_profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "text/plain, application/octet-stream, application/binary, text/csv, application/csv, application/excel, application/pdf, text/comma-separated-values, text/xml, application/xml, text/html; charset=utf-8")
-		ff_profile.set_preference("pdfjs.disabled", True)
-		ff_profile.set_preference("print.always_print_silent", True)
-		ff_profile.set_preference("dom.disable_open_during_load", False)
-		ff_profile.set_preference("browser.privatebrowsing.autostart", True)
-		ff_profile.set_preference("browser.privatebrowsing.autostart", True)
-		ff_profile.set_preference("general.useragent.override", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36")
+		firefox_profile = webdriver.FirefoxProfile()
+		firefox_profile.set_preference("browser.download.folderList", 2)
+		firefox_profile.set_preference("browser.download.manager.showWhenStarting", False)
+		firefox_profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "text/plain, application/octet-stream, application/binary, text/csv, application/csv, application/excel, application/pdf, text/comma-separated-values, text/xml, application/xml, text/html; charset=utf-8")
+		firefox_profile.set_preference("pdfjs.disabled", True)
+		firefox_profile.set_preference("print.always_print_silent", True)
+		firefox_profile.set_preference("dom.disable_open_during_load", False)
+		firefox_profile.set_preference("browser.privatebrowsing.autostart", True)
+		firefox_profile.set_preference("browser.privatebrowsing.autostart", True)
+		firefox_profile.set_preference("general.useragent.override", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36")
 
-		return ff_profile
+		return firefox_profile
 
 
 	def get_driver(self):
 
-		driver = webdriver.Firefox(executable_path=self.ff_driver_path, firefox_profile=self.ff_profile)
+		driver = webdriver.Firefox(executable_path=self.ff_driver_path, firefox_profile=self.firefox_profile)
 		driver.set_page_load_timeout(200)
 
 		return driver
