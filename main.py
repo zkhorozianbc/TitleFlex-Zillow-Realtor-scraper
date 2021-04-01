644a96fac934d45e7f748179dca15f770a8584a9
Zachary Khorozian <zkhorozianbc@gmail.com>
2021-04-01 13:44:19 -0400

    added argparser functionality

--- main.py
+++ main.py
@@ -2,38 +2,56 @@ from TitleFlex import TitleFlex
 from FirefoxDriver import FirefoxDriver
 import sys
 import json
-
-
-def parse_address_from_cmd_args():
-	#check if some address is provided
-	assert len(sys.argv) > 2
-
-	address = " ".join(sys.argv)
-
-	return address
+import argparse
 
 def write_property_data_to_file(property_data,filename="property_data.json"):
+
 	#creates json file from property_data dict and 
 	#saves in current directory
 	with open(filename, 'w') as f:
 		json.dump(property_data, f, indent=4, sort_keys=True)
 
-def main():
+def main(args):
+
 	#parse address from command line, throw error if none provided
-	address = parse_address_from_cmd_args()
+	address = " ".join(args.address)
+
 	## Firefox Webdriver with custom settings 
 	driver = FirefoxDriver().get_driver()
+
 	##create titleflex object
 	titleflex = TitleFlex(driver)
+
 	##Get property data from titleflex, zillow, realtor
 	property_data = titleflex.get_property_data(address)
+
 	#write property_data dict to property_data.json
 	write_property_data_to_file(property_data)
+
 	#close driver
 	driver.close()
 	
 
-
 #Get address from sys args 
 if __name__ == "__main__":
-	main()
+
+	parser = argparse.ArgumentParser()
+
+	parser.add_argument(
+	    "--address",
+		"-a",
+	    type=str,
+	    help="Address to search",
+	    nargs="+",
+	    required=True
+	)
+	parser.add_argument(
+	    "--verbose",
+		"-v",
+	    help="Print verbose logs",
+	    action='store_true'
+	)
+	
+	args = parser.parse_args()
+
+	main(args)
